ca920c967cd8a55630461943700f699db6e96fb5
Alex Robbins <alexrrobbins@gmail.com>
2019-03-16 15:39:34 -0400

    Initial commit

---
+++ Morse_Translate.py
@@ -0,0 +1,13 @@
+class Morse():
+
+    import time
+
+    def __int__(self):
+        input = []
+        elapsed_time = 0
+
+    def get_input(self,pin_input):
+        start = time.time()
+        input.append(pin_input)
+        stop = time.time()
+        elapsed_time += ( stop - start )
