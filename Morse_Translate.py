04702962e8471c7360c7702dc6eb5d4b9de106a2
Alex Robbins <alexrrobbins@gmail.com>
2019-03-16 15:51:19 -0400

    Added a method to implement time rules

--- Morse_Translate.py
+++ Morse_Translate.py
@@ -6,8 +6,28 @@ class Morse():
         input = []
         elapsed_time = 0
 
-    def get_input(self,pin_input):
-        start = time.time()
-        input.append(pin_input)
-        stop = time.time()
-        elapsed_time += ( stop - start )
+    def get_input(self, pin_input):
+        time_units = 0
+        while pin_input == 1:
+            start = time.time()
+            end = time.time()
+            time_units = end - start
+            if time_units == 1:
+                input.append( dot )
+            elif time_units == 3:
+                input.append( dash )
+        while pin_input == 0:
+            start = time.time()
+            end = time.time()
+            time_units = end - start
+            if time_units == 1:
+                break
+            elif time_units == 3:
+                input.append( eol )
+            elif time_units == 7:
+                input.append( eow )
+            elif time_units > 7:
+                translate_input
+
+    def translate_input(self):
+        pass
